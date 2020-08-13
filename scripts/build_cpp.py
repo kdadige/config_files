147dc38c76ae87a370950d3dbba5880ca16d4570
kdadige <dadige.keerthi@gmail.com>
2020-08-13 17:00:50 -0500

    vim plugins

--- scripts/build_cpp.py
+++ scripts/build_cpp.py
@@ -10,14 +10,19 @@ import subprocess
 def main(args, compile_file=''):
     if args.cpp11:
         file_name = os.path.join(os.environ['PWD'], args.cpp11)
-        exe_name = os.path.join(os.environ['PWD'], 'bin', args.cpp11.split(".")[0])
+        exe_name = os.path.join(os.environ['PWD'], args.cpp11.split(".")[0])
         params = ['/usr/bin/g++', '-std=c++11', '-o', exe_name, file_name]
+    elif args.cpp14:
+        file_name = os.path.join(os.environ['PWD'], args.cpp14)
+        exe_name = os.path.join(os.environ['PWD'], args.cpp14.split(".")[0])
+        params = ['/usr/bin/g++', '-std=c++14', '-o', exe_name, file_name]
     else:
         file_name = os.path.join(os.environ['PWD'], compile_file)
-        exe_name = os.path.join(os.environ['PWD'], 'bin', compile_file.split(".")[0])
-        params = ['/usr/bin/g++', '-std=c++11', '-o', exe_name, file_name]
-        params = ['/usr/bin/g++', '-o', exe_name, file_name]
+        exe_name = os.path.join(os.environ['PWD'], compile_file.split(".")[0])
+        params = ['/usr/bin/g++', '-std=c++14', '-O3', '-o',  exe_name, file_name]
+        #params = ['/usr/bin/g++', '-o', exe_name, file_name]
 
+    #print(params);
     try:
         subprocess.check_call(params)
     except subprocess.CalledProcessError as status:
@@ -27,6 +32,7 @@ def main(args, compile_file=''):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Execute C/C++ code')
     parser.add_argument('-11', '--cpp11', help='Compiler standard')
+    parser.add_argument('-14', '--cpp14', help='Compiler standard')
 
     if len(sys.argv) < 2:
         print('\nERROR  :  Enter the filename to compile.\n')
